f05f6989d20360b6d6e233ee7015f286d5f89374
swkima-popos <greyonth@gmail.com>
2026-02-20 15:02:22 +0900

    improve: Platformトレイトのlog_initにコメントを追加

--- protocolstack/domain/src/pal.rs
+++ protocolstack/domain/src/pal.rs
@@ -6,6 +6,7 @@ pub trait Platform {
     fn init() {
         Self::log_init(Self::create_logger());
     }
+    // The Log trait does not specify an initialization function, so it declares it as overridable.
     fn log_init(logger: Self::Logger);
 
     fn create_logger() -> Self::Logger;
